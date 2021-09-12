446c4930bb5f30d9dab4dc74644be56ad344c7c8
Oleksii83 <zaaytav@gmail.com>
2021-09-12 19:43:41 +0300

    task end
    fixed the addContact function ,     setContacts(prevContacts => ({
          contacts: [...prevContacts, contact],
        }));
        setContacts(prevContacts => [...prevContacts, contact]);

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import shortid from 'shortid';
 import ContactForm from './Component/ContactForm/ContactForm';
 import Filter from './Component/Filter/Filter';
 import ContactList from './Component/ContactList/ContactList';
+import useLocalStorage from './Component/LocalStorage/LocalStorage';
 
 export default function App() {
-  const [contacts, setContacts] = useState([
+  const [contacts, setContacts] = useLocalStorage('contacts', [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
@@ -37,21 +38,19 @@ export default function App() {
 
     if (equalName) return alert(`${contact.name} is already in contacts`);
 
-    setContacts(prevContacts => ({
-      contacts: [...prevContacts, contact],
-    }));
+    setContacts(prevContacts => [...prevContacts, contact]);
   };
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
 
-  useEffect(() => {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    if (contacts) {
-      setContacts(contacts);
-    }
-  }, []);
+  // useEffect(() => {
+  //   const contacts = JSON.parse(localStorage.getItem('contacts'));
+  //   if (contacts) {
+  //     setContacts(contacts);
+  //   }
+  // }, []);
 
   return (
     <>
